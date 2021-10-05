7086252dff2f8175f1b14325bc7d57ad959ac13e
JP Dewey <jpdeweymusic@gmail.com>
2021-10-04 20:20:54 -0400

    Code draft one and initial .md file

---
+++ Mice/DeweyMice.py
@@ -0,0 +1,12 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> str1 = input("Name: ")
+Name: Blind
+>>> str2 = input("Name: ")
+Name: Mice
+>>> print(f"\t{1+2} \n \t \t {str1} \n \t \t \t{str2}")
+	3 
+ 	 	 Blind 
+ 	 	 	Mice
+>>> 
